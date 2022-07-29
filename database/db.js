21841ad1d407f90647c294b8f315b0e766dd6500
Moses V <mov7963@gmail.com>
2022-07-29 14:34:39 -0400

    Fixed DB Connection

--- database/db.js
+++ database/db.js
@@ -1,8 +1,9 @@
-import postgres from 'postgres';
-const sql =
-  process.env.NODE_ENV === 'production'
-    ? // "Unless you're using a Private or Shield Heroku Postgres database, Heroku Postgres does not currently support verifiable certificates"
-      // https://help.heroku.com/3DELT3RK/why-can-t-my-third-party-utility-connect-to-heroku-postgres-with-ssl
-      postgres(process.env.DATABASE_URL,{ ssl: { rejectUnauthorized: false } })
-    : postgres('postgres://localhost:5432/firststore')
-export default sql ;
+import postgres from "postgres";
+const { DATABASE_URL, NODE_ENV } = process.env;
+const sql = postgres(
+  DATABASE_URL || "postgres://localhost:5432/firststore",
+  NODE_ENV === "production"
+    ? { ssl: { rejectUnauthorized: false }, idle_timeout: 60 }
+    : { idle_timeout: 60 }
+);
+export default sql;
