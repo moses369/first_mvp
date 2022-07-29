5dae390ccf9bc4cf3d122c31567422d0c9cf7830
Moses V <mov7963@gmail.com>
2022-07-29 13:36:21 -0400

    Fixed DB Connection

--- database/db.js
+++ database/db.js
@@ -1,6 +1,7 @@
 import postgres from 'postgres';
-const sql = postgres({
-   database:'firststore'
-})
+const sql = postgres(process.env.DATABSE_URL || 'postgres://localhost:5432/firststore',{
+   ssl:false
+}
+)
 
 export default sql ;
