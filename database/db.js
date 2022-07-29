21a6f2b532059f7a145af27ee05e61f4fe49d53b
Moses V <mov7963@gmail.com>
2022-07-29 13:47:51 -0400

    Hotfix DB

--- database/db.js
+++ database/db.js
@@ -1,7 +1,8 @@
 import postgres from 'postgres';
-const sql = postgres(process.env.DATABASE_URL || 'postgres://localhost:5432/firststore',{
-   ssl:false
-}
-)
-
+const sql =
+  process.env.NODE_ENV === 'production'
+    ? // "Unless you're using a Private or Shield Heroku Postgres database, Heroku Postgres does not currently support verifiable certificates"
+      // https://help.heroku.com/3DELT3RK/why-can-t-my-third-party-utility-connect-to-heroku-postgres-with-ssl
+      postgres(process.env.DATABASE_URL,{ ssl: { rejectUnauthorized: false } })
+    : postgres('postgres://localhost:5432/firststore')
 export default sql ;
