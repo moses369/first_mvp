129f8e90aa5727e5f441a0898644f26c9c69a1e4
Moses V <mov7963@gmail.com>
2022-07-30 12:59:56 -0400

    Connected to kroger api

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 import sql from "./database/db.js";
 import express from "express";
 import dotenv from "dotenv";
-import fetch from 'node-fetch';
-import token from './token'
+import products from './routes/product.js'
+import users from './routes/users.js'
+import cors from 'cors'
 
 dotenv.config();
 const app = express();
-const { PORT } = process.env;
+const { PORT, API_TOKEN } = process.env;
 
-const products = express.Router();
-const users = express.Router();
+
 
 const sendPost = async (req, res, next, table) => {
   try {
@@ -37,99 +37,12 @@ const sendPatch = async (req, res, next, table) => {
   }
 };
 
+app.use(cors())
+app.set('proxy server',1)
 app.use(express.static("static"));
 app.use(express.json());
 app.use("/api/products", products);
 app.use("/api/users", users);
-
-products
-  .route("/")
-  .get(async (req, res, next) => {
-    try { 
-   
-    } catch (err) {
-      next(err);
-    }
-  })
-  .post(
-    async (req, res, next) => {
-      try {
-        const { name, image, price, per_lb } = { ...req.body };
-
-        if (!name || !image || !price || !per_lb.toString()) {
-          res
-            .status(400)
-            .type("text")
-            .send(`Bad Request: name, image, price, per_lb keys required`);
-        } else {
-          next();
-        }
-      } catch (err) {
-        next(err);
-      }
-    },
-    (req, res, next) => sendPost(req, res, next, "products")
-  );
-
-users
-  .route("/")
-  .post(
-    async (req, res, next) => {
-      try {
-        const { name, email, phone, address } = { ...req.body };
-        const tel = parseInt(phone);
-        if (!name || !email || !address) {
-          res
-            .status(400)
-            .type("text")
-            .send(`Bad Request: name, email, address keys required`);
-        } else if (
-          (phone || phone === "") &&
-          (isNaN(tel) || tel.toString().length !== 10)
-        ) {
-          res
-            .status(400)
-            .type("text")
-            .send(`Bad Request: phone number must be valid`);
-        } else {
-          next();
-        }
-      } catch (err) {
-        next(err);
-      }
-    },
-    (req, res, next) => sendPost(req, res, next, "users")
-  )
-  .get(async (req, res, next) => {
-    try {
-      const users = await sql`
-          SELECT * FROM users ORDER BY id ASC`;
-      res.json(users);
-    } catch (err) {
-      next(err);
-    }
-  });
-
-users.route("/:id").patch(
-  async (req, res, next) => {
-    try {
-      const { id } = req.params;
-      if (isNaN(id)) {
-        next("route");
-      } else {
-        const item = (await sql`SELECT * FROM  users WHERE id = ${id} `)[0];
-        if (item) {
-          next();
-        } else {
-          next("route");
-        }
-      }
-    } catch (err) {
-      next(err);
-    }
-  },
-  async (req, res, next) => sendPatch(req, res, next, "users")
-);
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
@@ -137,7 +50,7 @@ app.use((err, req, res, next) => {
 });
 
 app.use((req, res) => {
-  res.status(404).type("text").send(`Page: ${req.url} Not Found `);
+  res.status(404).type("text").send(`Page Not Found `);
 });
 app.listen(PORT, () => {
   console.log(`SERVER live on localhost:${PORT}`);
--- token.js
+++ token.js
@@ -1,25 +1,23 @@
-import fetch from 'node-fetch';
-import btoa from 'btoa';
+import fetch from "node-fetch";
+import btoa from "btoa";
+import dotenv from "dotenv";
+dotenv.config();
+const { API_CLIENT_USER, API_CLIENT_SECRET } = process.env;
 const token = (async () => {
-   try {
-     const tokenHeaders ={
-        "Content-Type": "application/x-www-form-urlencoded",
-        "Authorization":`Basic ${btoa(
-         "firststore-d26c190990b4cfe3a6cb5994c78d98c13589844736430073127:5lCnQ74LlibedjKMhi9XK2gb5OllDIkyrp7VXwdU"
-       )}`
-     }
-     const res = await fetch("https://api.kroger.com/v1/connect/oauth2/token", {
-       method: "POST",
-       headers: tokenHeaders,
-       body: "grant_type=client_credentials",
-     });
-     const token = await res.json();
-     const { access_token } = token;
-     return access_token;
-   } catch (err) {
-     console.error(err);
-   }
- })();
-
-
- export default token
+  try {
+    const tokenHeaders = {
+      "Content-Type": "application/x-www-form-urlencoded",
+      Authorization: `Basic ${btoa(`${API_CLIENT_USER}:${API_CLIENT_SECRET}`)}`,
+    };
+    const res = await fetch("https://api.kroger.com/v1/connect/oauth2/token", {
+      method: "POST",
+      headers: tokenHeaders,
+      body: "grant_type=client_credentials&scope=product.compact",
+    });
+    const token = await res.json();
+    return token
+  } catch (err) {
+    console.error(err);
+  }
+})();
+export default token
